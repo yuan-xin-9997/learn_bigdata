283d80a09e90dfb7d85fbe4a8aa9b0b3096a1c2a
yuan.xin <yuanxin9997@qq.com>
2023-11-20 21:56:20 +0800

    20231120

--- Hello/Hello.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class Hello{
-	public static void main(String[] args){
-		System.out.println(args.length);
-        System.out.println(Arrays.toString(args));
-}
-}
---
+++ MRDemo1/src/main/java/org/atguigu/mr/wordcount/WCDriver.java
@@ -0,0 +1,7 @@
+package org.atguigu.mr.wordcount;
+
+/*
+* 程序入口，1.创建Job实例
+* */
+public class WCDriver {
+}
