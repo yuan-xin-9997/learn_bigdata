122c9b9634d392a56f787a078dbe3adbc1a4f31e
yuan.xin <yuanxin9997@qq.com>
2024-06-24 21:48:42 +0800

    flink allow late, side output, timer demo

---
+++ FlinkDemo1/src/main/java/com/atguigu/chapter7/timer/Flink02_Timer_ProcessTime.java
@@ -0,0 +1,86 @@
+package com.atguigu.chapter7.timer;
+
+import com.atguigu.chapter05_source.WaterSensor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author: yuan.xin
+ * @createTime: 2024/06/24 20:24
+ * @contact: yuanxin9997@qq.com
+ * @description: Flink 定时器  基于处理时间的定时器
+ *
+ * 基于处理时间或者事件时间处理过一个元素之后, 注册一个定时器, 然后指定的时间执行.
+ * 	Context和OnTimerContext所持有的TimerService对象拥有以下方法:
+ * currentProcessingTime(): Long 返回当前处理时间
+ * currentWatermark(): Long 返回当前watermark的时间戳
+ * registerProcessingTimeTimer(timestamp: Long): Unit 会注册当前key的processing time的定时器。当processing time到达定时时间时，触发timer。
+ * registerEventTimeTimer(timestamp: Long): Unit 会注册当前key的event time 定时器。当水位线大于等于定时器注册的时间时，触发定时器执行回调函数。
+ * deleteProcessingTimeTimer(timestamp: Long): Unit 删除之前注册处理时间定时器。如果没有这个时间戳的定时器，则不执行。
+ * deleteEventTimeTimer(timestamp: Long): Unit 删除之前注册的事件时间定时器，如果没有此时间戳的定时器，则不执行。
+ */
+public class Flink02_Timer_ProcessTime {
+    public static void main(String[] Args) {
+        // Web UI 端口设置
+        Configuration conf = new Configuration();
+        conf.setInteger("rest.port", 20000);
+
+        // 1. 创建流式执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+
+        // 设置并行度，如果不设置，默认并行度=CPU核心数
+        env.setParallelism(1);
+
+        // Flink程序主逻辑
+        env
+                .socketTextStream("hadoop102", 9999)
+                .map(line -> {
+                    String[] fields = line.split(",");
+                    return new WaterSensor(fields[0], Long.parseLong(fields[1]), Integer.parseInt(fields[2]));
+                })
+                // 当水位超过10，5s发出预警
+                // 定时器是和Key绑定在一起，必须先进行keyBy
+                // 如果只有1个并行度，不同key下的定时器只会影响自己key
+                .keyBy(WaterSensor::getId)
+                .process(new KeyedProcessFunction<String, WaterSensor, String>() {
+
+                    private long ts;
+
+                    @Override
+                    public void processElement(WaterSensor value, KeyedProcessFunction<String, WaterSensor, String>.Context ctx, Collector<String> out) throws Exception {
+                        if (value.getVc() > 10) {
+                            // 注册定时器，基于处理时间
+                            ts = System.currentTimeMillis() + 5000;
+                            System.out.println("注册定时器：" + ts);
+                            ctx.timerService().registerProcessingTimeTimer(ts);
+                        } else {
+                            // 删除定时器
+                            System.out.println("删除定时器：" + ts);
+                            ctx.timerService().deleteProcessingTimeTimer(ts);
+                        }
+
+                        // out.collect(value.toString());
+                    }
+
+                    // 定时器触发时执行
+                    @Override
+                    public void onTimer(long timestamp,  // 定时器触发时间
+                                        KeyedProcessFunction<String, WaterSensor, String>.OnTimerContext ctx,  // 上下文
+                                        Collector<String> out) throws Exception {  // 输出流
+                        out.collect(ctx.getCurrentKey() + "水位超过10，发出预警....");
+                    }
+
+                })
+                .print();
+
+        // 懒加载
+        try {
+            env.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
--- FlinkDemo1/src/main/java/com/atguigu/chapter7/watermark/Flink04_AllowLate.java
+++ FlinkDemo1/src/main/java/com/atguigu/chapter7/watermark/Flink04_AllowLate.java
@@ -2,7 +2,8 @@ package com.atguigu.chapter7.watermark;
 
 import com.atguigu.chapter05_source.WaterSensor;
 import com.atguigu.utils.AtguiguUtil;
-import org.apache.flink.api.common.eventtime.*;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
@@ -18,15 +19,18 @@ import java.util.List;
  * @author: yuan.xin
  * @createTime: 2024/06/20 19:23
  * @contact: yuanxin9997@qq.com
- * @description: Flink 水印  并行度不为1的情况下，水印的传递机制，传递水印最小的
+ * @description: Flink 水印  允许迟到
  *
- * 总结: 多并行度的条件下, 向下游传递WaterMark的时候, 总是以最小的那个WaterMark为准! 木桶原理!
- *
- * 多并行度下，数据倾斜会导致WaterMark不更新，解决方案：1.修改为单并行度（不好，降低系统处理性能），2. 解决数据倾斜问题（推荐），3. 设置最大空闲时间
- * 若某个并行度的水印在一段时间内没有更新，那么该并行度上的水印不会往下传递
- * 如果达到最大空闲时间，则水印会自动往后传递
+ * 7.4窗口允许迟到的数据
+ * 已经添加了wartemark之后, 仍有数据会迟到怎么办?  Flink的窗口, 也允许迟到数据.
+ * 当触发了窗口计算后, 会先计算当前的结果, 但是此时并不会关闭窗口.以后每来一条迟到数据, 则触发一次这条数据所在窗口计算(增量计算).
+ * 那么什么时候会真正的关闭窗口呢?  wartermark 超过了窗口结束时间+等待时间
+ * .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+ * .allowedLateness(Time.seconds(3))
+ * 注意:
+ * 允许迟到只能运用在event time上
  */
-public class Flink03_watermark_pall2 {
+public class Flink04_AllowLate {
     public static void main(String[] Args) {
         // Web UI 端口设置
         Configuration conf = new Configuration();
@@ -36,7 +40,7 @@ public class Flink03_watermark_pall2 {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
 
         // 设置并行度，如果不设置，默认并行度=CPU核心数
-        env.setParallelism(2);
+        env.setParallelism(1);
 
         // Flink程序主逻辑
         env.getConfig().setAutoWatermarkInterval(2000);  // 设置默认自动添加水印的时间, ms
@@ -48,18 +52,20 @@ public class Flink03_watermark_pall2 {
                 })
                 .assignTimestampsAndWatermarks(  // 分配时间戳和水印
                         WatermarkStrategy
-                                .<WaterSensor>forBoundedOutOfOrderness(Duration.ofSeconds(3))
+                                .<WaterSensor>forBoundedOutOfOrderness(Duration.ofSeconds(3))  // 最大容忍的延迟时间
                                 .withTimestampAssigner(new SerializableTimestampAssigner<WaterSensor>() {
                                     @Override
                                     public long extractTimestamp(WaterSensor element, long recordTimestamp) {
                                         return element.getTs();
                                     }
                                 })
-                                .withIdleness(Duration.ofSeconds(5))  // 设置最大空闲时间，解决数据倾斜导致水印不更新的问题
+                                //.withIdleness(Duration.ofSeconds(5))  // 设置最大空闲时间，解决数据倾斜导致水印不更新的问题
                 )
                 .keyBy(WaterSensor::getId)
                 .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .allowedLateness(Time.seconds(2))  // 允许迟到时间：2s，当到了窗口的关闭时间，窗口先不关闭，但会对窗口元素先做计算，过2s后窗口真正的关闭。再迟到的数据不会参与计算
                 .process(new ProcessWindowFunction<WaterSensor, String, String, TimeWindow>() {
+                    // 在允许迟到期间，每来1个属于这个窗口的元素，这个方法就会执行
                     @Override
                     public void process(String key,
                                         ProcessWindowFunction<WaterSensor, String, String, TimeWindow>.Context ctx,
